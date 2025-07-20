5fc8ef8565f4de71253f2f0bdbaa44583ce4934d
misha <kiritosan08anime@gmail.com>
2025-07-20 18:23:46 +0200

    upd

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+// !MENU
+const menu = document.querySelector(".btn-menu");
+const closeMenu = document.querySelector(".btn-close");
+const nav = document.querySelector(".box-nav");
+const header = document.querySelector(".header")
+
+menu.addEventListener("click", () => {
+  nav.classList.add("is-open");
+  document.body.classList.add("no-scroll");
+  goUpBtn.style.display = "none";
+  header.classList.add("no-shadow");
+});
+
+closeMenu.addEventListener("click", () => {
+  nav.classList.remove("is-open");
+  document.body.classList.remove("no-scroll");
+  goUpBtn.style.display = "block";
+  header.classList.remove("no-shadow");
+});
+
+// !MODAL WINDOW
+const orderButton = document.querySelector(".btn-order");
+const modalWindow = document.querySelector(".box-modal-bg");
+const closeModal = document.querySelector(".btn-modal");
+
+orderButton.addEventListener("click", () => {
+  modalWindow.classList.add("is-open");
+});
+
+closeModal.addEventListener("click", () => {
+  modalWindow.classList.remove("is-open");
+});
+
+// !HEADER BOX-SHADOW HIDIND
